cd8b6125040a40cb3604ba88253502a93cd1926d
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2023-01-12 12:13:36 +0600

    raw string

---
+++ .vscode/rawstring.dart
@@ -0,0 +1,7 @@
+main() {
+  num prize = 10;
+  String withoutRawString = 'The valus of prize is \t$prize';
+  String withRawString = r'The valus of prize is \t $prize';
+  print(withoutRawString);
+  print(withRawString);
+}
