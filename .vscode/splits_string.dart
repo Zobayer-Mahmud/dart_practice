a0e517dfdabd29cb915d27ea6895fea8cdcd0669
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2023-01-12 14:37:27 +0600

    split string to  list

---
+++ .vscode/splits_string.dart
@@ -0,0 +1,6 @@
+void main(){
+  String allFruits = 'Apple, Orange, Mango, Graps'
+  List<String> fruitsList =  allFruits.split(',');
+  print(allFruits);
+  print(fruitsList);
+}
---
+++ .vscode/string_property.dart
@@ -0,0 +1,12 @@
+void main() {
+  String str = " Hi ";
+  print(str.codeUnits);
+  print(str.trimRight());
+  print(str.trimLeft());
+  print(str.trim());
+  print(str.compareTo(" Hi "));
+  print(str.trim());
+  print(str.trim());
+  print(str.trim());
+  print(str.trim());
+}
