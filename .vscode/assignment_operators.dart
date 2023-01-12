fbbf69f584b3d63d893e8953ba8f196aaaefcdcf
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2023-01-12 13:00:00 +0600

    assignment_operators

---
+++ .vscode/assignment_operators.dart
@@ -0,0 +1,11 @@
+void main() {
+  double age = 24;
+  age += 1;
+  print("age add ${age.toInt()}");
+  age -= 1;
+  print("age sub ${age.toInt()}");
+  age *= 2;
+  print("age mul ${age.toInt()}");
+  age /= 2;
+  print("age div ${age.toInt()}");
+}
