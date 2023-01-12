1212d8d3d741e2da2142346f0e42f79cde66fbd6
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2023-01-12 13:43:49 +0600

    user from input

---
+++ .vscode/input_by_user.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  print("Enter name: .....");
+  String? name = stdin.readLineSync();
+  print("The entered namee is $name");
+  print("Enter Age: .....");
+  int? age = int.tryParse(stdin.readLineSync());
+  print("The entered age is ${age ?? 0}");
+  print("Enter double: .....");
+  double? value = double..tryParse(stdin.readLineSync());
+  print("The entered double is $value");
+}
