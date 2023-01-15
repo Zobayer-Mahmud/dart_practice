7db30302181d6072d976e31fbe451807c77ae800
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2023-01-15 12:11:06 +0600

    input user error

--- input_by_user.dart
+++ input_by_user.dart
@@ -5,9 +5,9 @@ void main() {
   String? name = stdin.readLineSync();
   print("The entered namee is $name");
   print("Enter Age: .....");
-  int? age = int.tryParse(stdin.readLineSync());
+  int? age = int.tryParse(stdin.readLineSync()!);
   print("The entered age is ${age ?? 0}");
   print("Enter double: .....");
-  double? value = double..tryParse(stdin.readLineSync());
+  double? value = double.tryParse(stdin.readLineSync()!);
   print("The entered double is $value");
 }
